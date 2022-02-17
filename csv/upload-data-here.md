35c93d56d6e06a2b0dc849a251961388620c53a7
hrosenskjold <47818937+hrosenskjold@users.noreply.github.com>
2022-02-17 08:23:27 +0100

    Initialize lauout+icon+attribute

---
+++ csv/upload-data-here.md
@@ -0,0 +1,2 @@
+Option: upload your Google Sheets data in separate CSV files here, as described in https://handsondataviz.org/leaflet-storymaps-with-google-sheets/
+[Kopi af Leaflet Storymaps with Google Sheets Template v1.2.0 - Chapters.csv](https://github.com/hrosenskjold/leaflet-storymaps-with-google-sheets/files/7949306/Kopi.af.Leaflet.Storymaps.with.Google.Sheets.Template.v1.2.0.-.Chapters.csv)
---
+++ google-doc-url.js
@@ -0,0 +1,9 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1cbchgKRIBUuN_Ix3txnJ6W0xo5SLKZRD2sfL97CKEWg/edit#gid=0';
+//                  https://docs.google.com/spreadsheets/d/1cbchgKRIBUuN_Ix3txnJ6W0xo5SLKZRD2sfL97CKEWg/edit#gid=0
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+//var googleApiKey = 'AIzaSyDCNki7i3Pcg7d04Yz7XNXV2nx5d2OG0QQ';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
